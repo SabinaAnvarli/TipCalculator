38c59e475fea6491b23ad915bc89760977f2429c
SabinaAnvarl <sabinaaam@code.edu.az>
2023-09-20 10:49:33 +0400

    Finish

---
+++ app.js
@@ -0,0 +1,13 @@
+var firstInput=document.querySelector(".firstInput")
+var rangeInput=document.querySelector(".rangeInput")
+var rangeValue=document.querySelector(".rangeValue")
+var amountValue=document.querySelector(".amountValue")
+var billValue=document.querySelector(".billValue")
+
+rangeInput.addEventListener("change",()=>{
+    console.log(firstInput)
+    rangeValue.innerText=`${rangeInput.value}%`
+    amountValue.innerText=`${rangeInput.value},00`
+    billValue.innerText=`${+firstInput.value+(+rangeInput.value)},00`
+   
+})
